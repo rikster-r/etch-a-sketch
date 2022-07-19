fbba8f9b76d2d2d4bd3d72bb774b958fd102a6e2
rikster-r <seitdanial43@gmail.com>
2022-07-19 18:09:11 +0600

    Add custom sizing + minor refactor

--- script.js
+++ script.js
@@ -1,18 +1,30 @@
 const board = document.querySelector('.sketchboard');
+const colorPicker = document.querySelector('.color-picker');
+const colorPickButton = document.querySelector('.color-custom');
+const rainbowButton = document.querySelector('.color-rainbow');
+const eraseButton = document.querySelector('.erase');
+const resetButton = document.querySelector('.reset');
+
 let mouseDown = false;
 let color = '#000';
 let usingRandomColor = false;
+let pixels = [];
+let currentWidth;
 
 function setSketchGrid(width) {
     let count = width * width;
+    let pixelWidth = board.offsetWidth/width;
+    pixels = [];
+
     for (let i = 1; i <= count; i++) {
         let div = document.createElement('div');
         div.classList.add('pixel');
-        div.style.width = div.style.height = board.offsetWidth/width;
-
-        board.append(div);
+        div.style.width = div.style.height = pixelWidth;
+        div.addEventListener('mouseover', (e) => paintPixel(e));
+        pixels.push(div);
     }
 
+    board.replaceChildren(...pixels);
     board.style.cssText = `grid-template-columns: repeat(${width}, ${100/width}%);`;
 }
 
@@ -36,11 +48,11 @@ function getRandomColor() {
 }
 
 function reset() {
-    pixels.forEach(elem => elem.style.backgroundColor = '#f6f6f6')
+    pixels.forEach(elem => elem.style.backgroundColor = '#f6f6f6');
 }
 
 
-document.addEventListener('DOMContentLoaded', setSketchGrid(16));
+document.addEventListener('DOMContentLoaded', setSketchGrid(25));
 document.addEventListener('mousedown', (e) => {
     e.preventDefault(); //stop browser dragging while drawing
     mouseDown = true;
@@ -48,20 +60,24 @@ document.addEventListener('mousedown', (e) => {
 });
 document.addEventListener('mouseup', () => mouseDown = false);
 
-let pixels = document.querySelectorAll('.pixel')
-pixels.forEach((elem) => elem.addEventListener('mouseover', (e) => paintPixel(e)))
 
-document.querySelector('.color-picker').addEventListener('input', (e) => {
+colorPicker.addEventListener('input', (e => {
     usingRandomColor = false;
-    changeColor(e.target.value) //change color to picked value
-}); 
-document.querySelector('.color-black').addEventListener('click', () => {
+    changeColor(e.target.value); //change color to picked value
+}));
+colorPickButton.addEventListener('click', () => {
     usingRandomColor = false;
-    changeColor('#000');
+    changeColor(colorPicker.value);
 });
-document.querySelector('.color-rainbow').addEventListener('click', useRandomColor)
-document.querySelector('.erase').addEventListener('click', () => {
+rainbowButton.addEventListener('click', useRandomColor)
+eraseButton.addEventListener('click', () => {
     usingRandomColor = false;
     changeColor('#f6f6f6'); //background color
-}) 
-document.querySelector('.reset').addEventListener('click', reset)
+});
+resetButton.addEventListener('click', reset);
+
+
+let sizePickers = document.querySelectorAll('.size-picker')
+sizePickers.forEach(button => button.addEventListener('click', (e) => {
+    setSketchGrid(Number(e.target.dataset.size));
+}));
